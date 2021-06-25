8b15aabffb31d43512923be408fadea11a68647c
海鹏 <lhp0305@vip.163.com>
2021-06-25 14:06:53 +0800

    init

--- src/main/java/com/heshen/plugin/HelloMojo.java
+++ src/main/java/com/heshen/plugin/HelloMojo.java
@@ -1,6 +1,7 @@
 package com.heshen.plugin;
 
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -36,6 +37,7 @@ public class HelloMojo extends AbstractMojo {
     @Parameter( defaultValue = "${project}")
     private MavenProject mavenProject;
 
+    @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         Class<? extends HelloMojo> aClass = getClass();
 
@@ -45,83 +47,17 @@ public class HelloMojo extends AbstractMojo {
         getLog().info("<==============河神说: Hello Mojo start==============>"+greeting);
 
         List dependencies = mavenProject.getDependencies();
-        getLog().info("[dependencies]");
-        for (Object dependency : dependencies) {
+        getLog().info("[dependencies]---->");
+        for (Object object : dependencies) {
+            Dependency dependency =  (Dependency)object;
             getLog().info(dependency.toString());
         }
-        getLog().info("[basedir]");
-        File basedir = mavenProject.getBasedir();
 
-        List<File> files = searchFiles(basedir, "java");
 
-        for (File file : files) {
-            long l = file.lastModified();
-            Date date = new Date(l);
-
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String format = dateFormat.format(date);
-            getLog().info(file.getName()+"[lastModified]"+format);
-        }
-
-        File file = searchJar(basedir);
-        getLog().info(">>>>>>>>jar:");
-        if (file!=null){
-            getLog().info(file.getName());
-        }
-
-        try {
-            Runtime.getRuntime().exec("touch aa 123");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         getLog().info("<==============河神说: Hello Mojo end==============>"+greeting);
 
 
     }
 
-    public static List<File> searchFiles(File folder, final String keyword) {
-        List<File> result = new ArrayList<File>();
-        if (folder.isFile()){
-            result.add(folder);
-        }
-        File[] subFolders = folder.listFiles((file) -> {
-            return file.getName().toLowerCase().endsWith(keyword);
-        });
-
-
-        if (subFolders != null) {
-            for (File file : subFolders) {
-                if (file.isFile()) {
-                    // 如果是文件则将文件添加到结果列表中
-                    result.add(file);
-                } else {
-                    // 如果是文件夹，则递归调用本方法，然后把所有的文件加到结果列表中
-                    result.addAll(searchFiles(file, keyword));
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public static File searchJar(File folder) {
-
-        File[] files = folder.listFiles((file) -> {
-            return file.isDirectory() && "target".equals(file.getName().toLowerCase());
-        });
-
-        if (files != null&&files.length>0) {
-            File target = files[0];
-            File[] subFolders = target.listFiles((file) -> {
-                return file.getName().toLowerCase().endsWith(".jar");
-            });
-
-            if (subFolders != null&&subFolders.length>0) {
-                return subFolders[0];
-            }
-            return null;
-        }
-        return null;
-    }
 }
